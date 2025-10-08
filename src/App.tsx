00411afd35ed9f76197a0671a3c73edb812499b5
TheAshenWitch <jaholamark410@gmail.com>
2025-10-08 10:58:49 +0200

    Pages
    Added the pages to the site and some compontents like a navbar to navigate between them

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import Test from './app/test/page';
-import Test2 from './app/test2/page';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './app/Home/page';
+import Warehouses from './app/Warehouses/page';
+import Admin from './app/Admin/page';
+import Statistics from './app/Statistics/page';
+import Profile from './app/Profile/page';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
-          }
-        />
-        <Route path="/test" element={<Test/>} />
-        <Route path="/test2" element={<Test2/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/warehouse" element={<Warehouses />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/profile" element={<Profile />} />
+        
       </Routes>
     </Router>
   );
---
+++ src/app/Admin/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import '../../App.css';
+import PillNavFull from '../../Components/NavBar/PillNav/PillNavWithItems';
+
+function Admin() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PillNavFull/>
+        ÁDMIN
+      </header>
+    </div>
+  );
+}
+
+export default Admin;
